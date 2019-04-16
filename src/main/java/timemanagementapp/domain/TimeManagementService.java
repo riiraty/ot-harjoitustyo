d05ad5c592a5c76d6ed55c4fe9fcf1ef1bc00add
Riikka Raty <48516441+riiraty@users.noreply.github.com>
2019-04-16 14:46:42 +0300

    Add javadocs

--- src/main/java/timemanagementapp/domain/TimeManagementService.java
+++ src/main/java/timemanagementapp/domain/TimeManagementService.java
@@ -25,10 +25,19 @@ public class TimeManagementService {
 //        this.logDao = logDao;
 //    }
 
+    /**
+     * Returns an ArrayList where the users logs are stored
+     * @return ArrayList<>
+     */
+
     public ArrayList<Log> getLogs() {
         return logs;
     }
     
+    /**
+     * Returns an ArrayList with all the ActivityTypes created
+     * @return ArrayList<>
+     */
     public ArrayList<ActivityType> getActivityTypes() {
         return activityTypes;
     }
@@ -37,8 +46,8 @@ public class TimeManagementService {
     /**
      * Creates a new log and ads it to the list that stores the logs
      * @param activityType type of the new log
-     * @param start
-     * @param end
+     * @param start moment the activity started at
+     * @param end moment the activity was finished
      * @return true 
      */
     public boolean createLog(ActivityType activityType, LocalDateTime start, LocalDateTime end) {
@@ -54,6 +63,12 @@ public class TimeManagementService {
         return true;
     }
     
+    /**
+     * Creates a new ActivityType and ads it to the ArrayList that stores the types
+     * @param type descriptive word for the new type
+     * @param color chosen colour for the type
+     * @return true
+     */
     public boolean createNewActivityType(String type, Color color) {
         activityTypes.add(new ActivityType(type, color));
         return true;
